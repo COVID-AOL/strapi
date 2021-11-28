b266d3d8603f9b6c313814666e0619601bd2ab5a
knkbga@prod <kanakbagga1@gmail.com>
2021-11-28 10:17:21 +0100

    middleware added for security

---
+++ config/middleware.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      enabled: true,
+      // headers: '*', 
+      origin: '*',
+      // origin: ["http://localhost", 'https://getcovidhelp.online', 'https://www.getcovidhelp.online'],
+    },
+  },
+};
